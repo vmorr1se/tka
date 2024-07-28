a807975fdfabd099d366c997238c0b9be344439a
vmorr1se <vaniazpq@gmail.com>
2024-07-28 20:46:06 +0700

    Delete assets/js/script BG ABOUT.js

--- assets/js/script BG ABOUT.js
+++
@@ -1,5 +0,0 @@
-const interactive = document.querySelector('.interactive');
-
-interactive.addEventListener('click', function() {
-    this.classList.toggle('search-mode'); // Toggle class saat diklik
-});
